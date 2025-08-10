b0d88f0c471fc1e1bb86e1eceb71ecbb07c1096b
chintaman2004 <ahmed.hassanc@gmail.com>
2025-08-09 23:00:34 -0700

    Daraz Replica Created

---
+++ darazreplica/android/app/src/main/kotlin/com/example/darazreplica/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.darazreplica
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- fbreplica/android/app/src/main/kotlin/com/example/fbreplica/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.fbreplica
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
